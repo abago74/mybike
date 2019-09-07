b1f1adf7d6ae31ed3bb90dcb4ce68c2eaed090f5
mymbp <mymbp@MBPdemymbp169.home>
2019-09-07 19:03:16 +0200

    2.3.5

--- fiido/fiido.ino
+++ fiido/fiido.ino
@@ -1,4 +1,4 @@
-const String VERSION = "V_2.3.4RC";
+const String VERSION = "V_2.3.5";
 const boolean traceOn=false;
 
 #include <EEPROM.h>
@@ -265,17 +265,18 @@ void loop() {
   }
   updatePower(); // actualizamos la potencia de salida.
 
-  /*plotter*/
+/*
+  //plotter/
   Serial.print(currentAngle*100);
   Serial.print("\t");
     
-  /*plotter*/
+  //plotter
   Serial.print(maxPowerValue);
   Serial.print("\t");
   
-  /*plotter*/
+  //plotter
   Serial.println(currentPowerValue);
-
+*/
 }
 
 // Interruption Methods ***********************************************************************************
@@ -813,8 +814,8 @@ void ATCommandsManager() {
         oled1306.print(F("> maxPowerAngle: "));
         oled1306.print(eStorage.maxPowerAngle);
 
-      } else if (command.indexOf("at+level") > -1) { // Calibrar posición de placa.
-        // TODO
+      } else if (command.indexOf(F("at+mpucalibrate")) > -1) { // Calibrar posición de placa.
+        calibrate(mpu6050);
       }
 
     }
@@ -838,4 +839,124 @@ String getAtData(String data, char separator, int index) { // Split string and g
     }
   }
   return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void calibrate(MPU6050 sensor) {
+
+  // Acelerometer
+  int ax, ay, az; // Raw acel
+  long f_ax,f_ay, f_az;
+  int p_ax, p_ay, p_az;
+  
+  // Gyro
+  int gx, gy, gz; // Raw giro
+  long f_gx,f_gy, f_gz;
+  int p_gx, p_gy, p_gz;
+  
+  // Offsets
+  int ax_o,ay_o,az_o; //offsets
+  int gx_o,gy_o,gz_o;
+
+  int counter=0;
+
+  // Read mpu values
+  ax_o=sensor.getXAccelOffset();
+  ay_o=sensor.getYAccelOffset();
+  az_o=sensor.getZAccelOffset();
+  gx_o=sensor.getXGyroOffset();
+  gy_o=sensor.getYGyroOffset();
+  gz_o=sensor.getZGyroOffset();
+  
+  long inittime = millis();
+  boolean calibdone=true;
+  while(calibdone){
+    // Leer las aceleraciones y velocidades angulares
+    sensor.getAcceleration(&ax, &ay, &az);
+    sensor.getRotation(&gx, &gy, &gz);
+  
+    // Filtrar las lecturas
+    f_ax = f_ax-(f_ax>>5)+ax;
+    p_ax = f_ax>>5;
+  
+    f_ay = f_ay-(f_ay>>5)+ay;
+    p_ay = f_ay>>5;
+  
+    f_az = f_az-(f_az>>5)+az;
+    p_az = f_az>>5;
+  
+    f_gx = f_gx-(f_gx>>3)+gx;
+    p_gx = f_gx>>3;
+  
+    f_gy = f_gy-(f_gy>>3)+gy;
+    p_gy = f_gy>>3;
+  
+    f_gz = f_gz-(f_gz>>3)+gz;
+    p_gz = f_gz>>3;
+  
+    //Cada 100 lecturas corregir el offset
+    if (counter==100){
+      //Mostrar las lecturas separadas por un [tab]
+      oled1306.clearDisplay();
+      oled1306.setCursor(0, 0);
+      oled1306.print(p_ax);
+      oled1306.print("|");
+      oled1306.print(p_ay);
+      oled1306.print("|");
+      oled1306.print(p_az);
+      oled1306.setCursor(0, 10);      
+      oled1306.print(p_gx);
+      oled1306.print("|");
+      oled1306.print(p_gy);
+      oled1306.print("|");
+      oled1306.println(p_gz);
+  
+      //Calibrar el acelerometro a 1g en el eje z (ajustar el offset)
+      if (p_ax>0) 
+        ax_o--;
+      else 
+        ax_o++;
+        
+      if (p_ay>0) 
+        ay_o--;
+      else 
+        ay_o++;
+      if (p_az-16384>0) 
+        az_o--;
+      else 
+        az_o++;
+      
+      sensor.setXAccelOffset(ax_o);
+      sensor.setYAccelOffset(ay_o);
+      sensor.setZAccelOffset(az_o);
+  
+      //Calibrar el giroscopio a 0º/s en todos los ejes (ajustar el offset)
+      if (p_gx>0) 
+        gx_o--;
+      else 
+        gx_o++;
+      if (p_gy>0) 
+        gy_o--;
+      else 
+        gy_o++;
+      if (p_gz>0) 
+        gz_o--;
+      else 
+        gz_o++;
+      
+      sensor.setXGyroOffset(gx_o);
+      sensor.setYGyroOffset(gy_o);
+      sensor.setZGyroOffset(gz_o);    
+  
+      //0 0 +16384
+      if(p_ax <= 5 && p_ax >= -5 && p_ay <= 5 && p_ay >= -5 && p_az <= 16394 && p_az >= 16374){
+        oled1306.setCursor(0, 20);      
+        oled1306.print("\t Calibration Done ");      
+        oled1306.print((millis()-inittime)/1000);
+        oled1306.print("s");  
+        calibdone=false;  
+      }
+      counter=0;
+    }
+    counter++;
+  }
 }
